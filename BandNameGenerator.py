0d3da4260070ae333265a204d36223486295f9c5
Yevhenii <yevhenii@MacBook-Air-Yevhenii.local>
2023-02-18 23:08:27 +0200

    Band name generator

---
+++ BandNameGenerator.py
@@ -0,0 +1,10 @@
+greetings = "Welcome to the Band Name Generator."
+question_about_city = "What's name of the city you grow up in? "
+question_about_pet = "What's your pet's name? "
+answer = "Your band name could be "
+
+print(greetings)
+city = input(question_about_city)
+pet = input(question_about_pet)
+print(answer + city + " " + pet)
+
